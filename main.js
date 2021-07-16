39772af81021aba2d6094b0d820da7788f10e805
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-17 01:26:43 +0700

    finish create and read using vue

--- main.js
+++ main.js
@@ -1,107 +1,137 @@
-const socket = new WebSocket("ws://localhost:3000");
-
-function sendMessageUpdate() {
-  socket.send("update");
-}
-
-function submitLogin(event) {
-  event.preventDefault();
-
-  let email = $("#loginEmail").val();
-  let password = $("#loginPassword").val();
-  localStorage.setItem("username", email.split("@")[0]);
-
-  $.ajax({
-    url: "http://localhost:3000/user/login",
-    method: "POST",
-    data: {
-      email,
-      password,
+const app = new Vue({
+  el: "#app",
+  data: {
+    isLogin: false,
+    isRegistering: false,
+    errorMessage: "",
+    isAddingTask: false,
+    tasks: [],
+    user: {
+      email: "",
+      password: "",
     },
-  })
-    .done((data) => {
-      localStorage.setItem("access_token", data.access_token);
-    })
-    .fail((error) => {
-      console.log(error);
-    })
-    .always((_) => {
-      $("#loginEmail").val("");
-      $("#loginPassword").val("");
-    });
-}
-
-function submitTask(event) {
-  event.preventDefault();
-
-  let title = $("#title").val();
-  let description = $("#description").val();
-  let priority = $("#priority").val();
-  let category = $("#category").val();
-  let due_date = $("#due_date").val().replace("T", " ") + ":00";
-
-  $.ajax({
-    url: "http://localhost:3000/task",
-    method: "POST",
-    headers: {
-      access_token: localStorage.access_token,
+    task: {
+      title: "",
+      description: "",
+      priority: "",
+      category: "",
+      due_date: "",
     },
-    data: {
-      title,
-      description,
-      priority,
-      category,
-      due_date,
-    },
-  })
-    .done(() => {
-      $("#title").val("");
-      $("#description").val("");
-      $("#priority").val("");
-      $("#category").val("");
-      $("#due_date").val("");
-      sendMessageUpdate();
-      getTasks();
-    })
-    .fail((error) => {
-      console.log(error);
-    });
-}
-
-function getTasks() {
-  if (localStorage.access_token) {
-    $.ajax({
-      url: "http://localhost:3000/task",
-      method: "GET",
-      headers: {
-        access_token: localStorage.access_token,
-      },
-    })
-      .done((results) => {
-        $("#tasks").empty();
-        results.forEach((result) => {
-          $("#tasks").append(`
-            <div>
-                <h5>${result.title}</h3>
-            </div>     
-            `);
-        });
+  },
+  methods: {
+    submitLogin() {
+      axios({
+        method: "POST",
+        url: "http://localhost:3000/user/login",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: this.user,
       })
-      .fail((err) => {});
-  }
-}
-
-$(document).ready(() => {
-  socket.addEventListener("open", function (event) {
-    console.log("connected to server");
-  });
-  socket.addEventListener("message", function (event) {
-    if (event.data === "update") {
-      getTasks();
-    }
-  });
-  $("#form-login").submit(submitLogin);
-  $("#form-add").submit(submitTask);
-
-  getTasks();
+        .then(({ data }) => {
+          localStorage.access_token = data.access_token;
+          this.isLogin = true;
+          this.getTasks();
+        })
+        .catch((err) => {
+          this.errorMessage = err.response.data.error[0];
+        })
+        .finally(() => {
+          this.user.email = "";
+          this.user.password = "";
+        });
+    },
+    submitRegister() {
+      axios({
+        method: "POST",
+        url: "http://localhost:3000/user/register",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: this.user,
+      })
+        .then(({ data }) => {
+          this.isRegistering = false;
+        })
+        .catch((err) => {
+          this.errorMessage = err.response.data.error[0];
+        })
+        .finally(() => {
+          this.user.email = "";
+          this.user.password = "";
+          this.errorMessage = "";
+        });
+    },
+    getTasks() {
+      axios({
+        method: "GET",
+        url: "http://localhost:3000/task",
+        headers: {
+          "Content-Type": "application/json",
+          access_token: localStorage.access_token,
+        },
+      })
+        .then(({ data }) => {
+          this.tasks = data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    submitTask() {
+      axios({
+        method: "POST",
+        url: "http://localhost:3000/task",
+        headers: {
+          "Content-Type": "application/json",
+          access_token: localStorage.access_token,
+        },
+        data: this.task,
+      })
+        .then(({ data }) => {
+          this.errorMessage = "";
+          this.isAddingTask = false;
+          this.getTasks();
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+        })
+        .finally(() => {
+          this.task = {
+            title: "",
+            description: "",
+            priority: "",
+            category: "",
+            due_date: "",
+          };
+        });
+    },
+    showRegisterForm() {
+      this.isRegistering = true;
+    },
+    showAddForm() {
+      this.isAddingTask = true;
+    },
+    logout() {
+      localStorage.removeItem("access_token");
+      this.isLogin = false;
+    },
+    adjustDate(e) {
+      this.task.due_date = e.target.value.replace("T", " ") + ":00";
+    },
+  },
+  computed: {
+    backlogTasks: function () {
+      return this.tasks.filter((task) => task.category === "backlog");
+    },
+    todoTasks: function () {
+      return this.tasks.filter((task) => task.category === "todo");
+    },
+    inProgressTasks: function () {
+      return this.tasks.filter((task) => task.category === "in progress");
+    },
+    doneTasks: function () {
+      return this.tasks.filter((task) => task.category === "done");
+    },
+  },
 });
