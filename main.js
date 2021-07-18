2f0387c6fbdbdf0aa942724a2b496bb4bfe99f92
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-18 18:53:52 +0700

    finish basic crud feature for front end

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const app = new Vue({
   el: "#app",
   data: {
-    isLogin: false,
+    isLogin: localStorage.access_token ? true : false,
     isRegistering: false,
-    errorMessage: "",
     isAddingTask: false,
+    isEditingTask: false,
+    errorMessage: "",
     tasks: [],
     user: {
       email: "",
       password: "",
     },
     task: {
+      id: 0,
       title: "",
       description: "",
       priority: "",
@@ -54,29 +56,41 @@ const app = new Vue({
           this.isRegistering = false;
         })
         .catch((err) => {
-          this.errorMessage = err.response.data.error[0];
+          if (
+            err.response.status === 400 &&
+            err.response.data.error[0] === "Email is not unique"
+          ) {
+            this.errorMessage =
+              "The email you registering has already been used";
+          } else if (err.response.status === 400) {
+            this.errorMessage = "Invalid Email or Password";
+          }
         })
         .finally(() => {
           this.user.email = "";
           this.user.password = "";
-          this.errorMessage = "";
+          if (!this.isRegistering) {
+            this.errorMessage = "";
+          }
         });
     },
     getTasks() {
-      axios({
-        method: "GET",
-        url: "http://localhost:3000/task",
-        headers: {
-          "Content-Type": "application/json",
-          access_token: localStorage.access_token,
-        },
-      })
-        .then(({ data }) => {
-          this.tasks = data;
+      if (this.isLogin) {
+        axios({
+          method: "GET",
+          url: "http://localhost:3000/task",
+          headers: {
+            "Content-Type": "application/json",
+            access_token: localStorage.access_token,
+          },
         })
-        .catch((err) => {
-          console.log(err);
-        });
+          .then(({ data }) => {
+            this.tasks = data;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     },
     submitTask() {
       axios({
@@ -94,7 +108,7 @@ const app = new Vue({
           this.getTasks();
         })
         .catch((err) => {
-          console.log(err.response.data);
+          this.errorMessage = err.response.data.error[0];
         })
         .finally(() => {
           this.task = {
@@ -106,32 +120,273 @@ const app = new Vue({
           };
         });
     },
+    submitEditTask(id) {
+      axios({
+        method: "PUT",
+        url: `http://localhost:3000/task/${id}`,
+        headers: {
+          "Content-Type": "application/json",
+          access_token: localStorage.access_token,
+        },
+        data: this.task,
+      })
+        .then(({ data }) => {
+          this.errorMessage = "";
+          this.isEditingTask = false;
+          this.getTasks();
+        })
+        .catch((err) => {
+          this.errorMessage = err.response.data.error[0];
+        });
+    },
+    deleteTask(id) {
+      axios({
+        method: "DELETE",
+        url: `http://localhost:3000/task/${id}`,
+        headers: {
+          "Content-Type": "application/json",
+          access_token: localStorage.access_token,
+        },
+      })
+        .then(() => {
+          this.errorMessage = "";
+          this.getTasks();
+        })
+        .catch((err) => {
+          this.errorMessage = err.response.data.error[0];
+        });
+    },
     showRegisterForm() {
+      if (!this.isRegistering) {
+        this.errorMessage = "";
+      }
       this.isRegistering = true;
     },
     showAddForm() {
+      if (!this.isAddingTask) {
+        this.errorMessage = "";
+      }
       this.isAddingTask = true;
+    },
+    showEditForm(id) {
+      axios({
+        method: "GET",
+        url: `http://localhost:3000/task/${id}`,
+        headers: {
+          "Content-Type": "application/json",
+          access_token: localStorage.access_token,
+        },
+      }).then((result) => {
+        result = result.data;
+        this.task.id = result.id;
+        this.task.title = result.title;
+        this.task.description = result.description;
+        this.task.priority = result.priority;
+        this.task.category = result.category;
+        this.task.due_date = this.adjustDateForEditForm(result.due_date);
+        this.isEditingTask = true;
+      });
+    },
+    backToLogin() {
+      this.isRegistering = false;
+    },
+    backToHome() {
+      this.errorMessage = "";
+      this.task.id = 0;
+      this.task.title = "";
+      this.description = "";
+      this.priority = "";
+      this.category = "";
+      this.due_date = "";
+      this.isAddingTask = false;
+      this.isEditingTask = false;
     },
     logout() {
       localStorage.removeItem("access_token");
       this.isLogin = false;
     },
-    adjustDate(e) {
+    adjustDateForSubmission(e) {
       this.task.due_date = e.target.value.replace("T", " ") + ":00";
+    },
+    adjustDateForMainPage(date) {
+      let dateEdited = String(date).split("T")[0];
+      let timeEdited = String(date).split("T")[1].slice(0, 5);
+      return dateEdited + " " + timeEdited;
+    },
+    adjustDateForEditForm(date) {
+      let dateEdited = String(date).split("T")[0];
+      let timeEdited = String(date).split("T")[1].slice(0, 5);
+      return dateEdited + "T" + timeEdited;
+    },
+    getPriorityColor(priority) {
+      switch (priority) {
+        case "high":
+          return "bg-red-400 h-2";
+          break;
+        case "medium":
+          return "bg-yellow-400 h-2";
+          break;
+        case "low":
+          return "bg-green-400 h-2";
+          break;
+      }
     },
   },
   computed: {
     backlogTasks: function () {
-      return this.tasks.filter((task) => task.category === "backlog");
+      let backlogs = this.tasks.filter((task) => task.category === "backlog");
+      backlogs = backlogs.map((backlog) => {
+        switch (backlog.priority) {
+          case "low":
+            backlog.priority = 1;
+            break;
+          case "medium":
+            backlog.priority = 2;
+            break;
+          case "high":
+            backlog.priority = 3;
+            break;
+        }
+        return backlog;
+      });
+      backlogs.sort(function (a, b) {
+        return (
+          String(a.due_date).localeCompare(String(b.due_date)) ||
+          b.priority - a.priority
+        );
+      });
+      backlogs = backlogs.map((backlog) => {
+        switch (backlog.priority) {
+          case 1:
+            backlog.priority = "low";
+            break;
+          case 2:
+            backlog.priority = "medium";
+            break;
+          case 3:
+            backlog.priority = "high";
+            break;
+        }
+        return backlog;
+      });
+      return backlogs;
     },
     todoTasks: function () {
-      return this.tasks.filter((task) => task.category === "todo");
+      let todos = this.tasks.filter((task) => task.category === "todo");
+      todos = todos.map((todo) => {
+        switch (todo.priority) {
+          case "low":
+            todo.priority = 1;
+            break;
+          case "medium":
+            todo.priority = 2;
+            break;
+          case "high":
+            todo.priority = 3;
+            break;
+        }
+        return todo;
+      });
+      todos.sort(function (a, b) {
+        return (
+          String(a.due_date).localeCompare(String(b.due_date)) ||
+          b.priority - a.priority
+        );
+      });
+      todos = todos.map((todo) => {
+        switch (todo.priority) {
+          case 1:
+            todo.priority = "low";
+            break;
+          case 2:
+            todo.priority = "medium";
+            break;
+          case 3:
+            todo.priority = "high";
+            break;
+        }
+        return todo;
+      });
+      return todos;
     },
     inProgressTasks: function () {
-      return this.tasks.filter((task) => task.category === "in progress");
+      let tasks = this.tasks.filter((task) => task.category === "in progress");
+      tasks = tasks.map((task) => {
+        switch (task.priority) {
+          case "low":
+            task.priority = 1;
+            break;
+          case "medium":
+            task.priority = 2;
+            break;
+          case "high":
+            task.priority = 3;
+            break;
+        }
+        return task;
+      });
+      tasks.sort(function (a, b) {
+        return (
+          String(a.due_date).localeCompare(String(b.due_date)) ||
+          b.priority - a.priority
+        );
+      });
+      tasks = tasks.map((task) => {
+        switch (task.priority) {
+          case 1:
+            task.priority = "low";
+            break;
+          case 2:
+            task.priority = "medium";
+            break;
+          case 3:
+            task.priority = "high";
+            break;
+        }
+        return task;
+      });
+      return tasks;
     },
     doneTasks: function () {
-      return this.tasks.filter((task) => task.category === "done");
+      let tasks = this.tasks.filter((task) => task.category === "done");
+      tasks = tasks.map((task) => {
+        switch (task.priority) {
+          case "low":
+            task.priority = 1;
+            break;
+          case "medium":
+            task.priority = 2;
+            break;
+          case "high":
+            task.priority = 3;
+            break;
+        }
+        return task;
+      });
+      tasks.sort(function (a, b) {
+        return (
+          String(a.due_date).localeCompare(String(b.due_date)) ||
+          b.priority - a.priority
+        );
+      });
+      tasks = tasks.map((task) => {
+        switch (task.priority) {
+          case 1:
+            task.priority = "low";
+            break;
+          case 2:
+            task.priority = "medium";
+            break;
+          case 3:
+            task.priority = "high";
+            break;
+        }
+        return task;
+      });
+      return tasks;
     },
+  },
+  mounted() {
+    this.getTasks();
   },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primarycolor: "#F9A826"
+      },
+      fontFamily: {
+        display: ["Nunito", "sans-serif"]
+      },
+      height: theme => ({
+        "screen/2": "50vh",
+        "screen/2+": "60vh",
+        "screen/2++": "67.5vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/3+": "calc(100vh / 3 + 2vh)",
+        "screen/3++": "calc(100vh / 3 + 5vh)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+        "screen3/4": "calc(100vh * 3 / 4)",
+        "screen3/4+": "calc(100vh * 3 / 4) + 2vh",
+      }),
+    },
   },
   variants: {
     extend: {},
