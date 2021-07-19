6871c3226a2442cd1aa7ec32d6e86004ea325cf4
Arip Budiman <aripbudiman007@gmail.com>
2021-07-19 21:33:33 +0700

    initial configuration

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'App',
+    data(){
+        return{
+            title: "This is my App"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
