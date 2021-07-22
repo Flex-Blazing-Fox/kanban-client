0a3503b0c760fa51694ac7c8a2dfd440fc87eb50
Arip Budiman <aripbudiman007@gmail.com>
2021-07-22 15:39:51 +0700

    add board and card component

---
+++ src/components/Board.vue
@@ -0,0 +1,124 @@
+<template>    
+    <div class="ui four wide column">
+        <div class="ui card board-category">
+            <div class="content">
+                <div class="board-header">{{ board.name }}</div>
+            </div>
+            <div class="content ">
+                
+                <draggable :list="taskList[board.name]" ghost-class="gost-card" v-bind="dragOptions" @change="log" group="all-tasks"  :move="onMove"  @start="isDragging=true" @end="isDragging=false">
+                    <!-- {{taskList[board.name]}} -->
+                    <Card v-for="task in taskList[board.name]" :key="task.id" :taskData="task" class="move" :name="'flip-list'"></Card>
+                </draggable>
+                <!-- <draggable v-model="taskList[board.name]" v-bind="dragOptions" :move="onMove"  @start="isDragging=true" @end="isDragging=false">
+                    <transition-group type="transition" :name="'flip-list'">
+                    <Card v-for="task in taskList[board.name]" :key="task.id" :taskData="task" class="move"></Card>
+                    </transition-group>
+                </draggable> -->
+            </div>
+            <div class="ui bottom green attached button">
+                <i class="add icon"></i>
+                Add Task
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import draggable from "vuedraggable"
+import Card from "../components/Card.vue"
+
+export default {
+    name:"Board",
+    props: ['board', 'taskList'],
+    components:{ Card, draggable },
+    data(){
+        return{
+            editable: true,
+            isDragging: false,
+            delayedDragging: false,
+           
+        }
+    },
+    computed:{
+        dragOptions() {
+            return {
+                animation: 0,
+                group: "description",
+                disabled: !this.editable,
+                ghostClass: "ghost"
+            };
+        },
+    },
+    watch: {
+    isDragging(newValue) {
+      if (newValue) {
+        this.delayedDragging = true;
+        return;
+      }
+      this.$nextTick(() => {
+        this.delayedDragging = false;
+      });
+    }
+  },
+    methods:{
+        onMove({ relatedContext, draggedContext }) {
+            console.log(relatedContext);
+            const relatedElement = relatedContext.element;
+            const draggedElement = draggedContext.element;
+            return (
+                (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+            );
+        },
+        log(event) {
+            console.log(event);
+           if(event.removed){
+               console.log(event.removed.element.id)
+           }
+
+           if(event.moved){
+               console.log(event.moved.element.id)
+           }
+
+           if(event.added){
+               console.log(event.added.element.id)
+           }
+        },
+    },
+    created(){
+        
+    }
+}
+</script>
+
+<style scoped>
+section{
+    height: 100vh;
+    display:flex;
+    
+    justify-content:space-evenly;
+}
+
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.board-header{
+    color:rgb(255, 255, 255);
+    font-size:12pt;
+    font-weight: bold;
+    text-shadow: 1px 1px #000000;
+    letter-spacing: 1px;
+    font-family: 'Montserrat', sans-serif;
+}
+.ui.card.board-category{
+    color:teal;
+    background: rgba(244, 244, 244, 0.2);
+}
+.ghost-card {
+  opacity: 0.5;
+  background: #F7FAFC;
+  border: 1px solid #4299e1;
+}
+.move {cursor: move;}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="ui cards">
+        <div class="ui card card-item">
+            <div class="content">
+                <div>
+                    <i class="left floated edit icon"></i>
+                    <i class="right floated close icon"></i>
+                </div>
+            </div>
+            <div class="content description">                
+                <p>
+                    {{taskData.title}}
+                </p>
+            </div>
+            <div class="extra content">
+                <div class="left floated author">
+                    <img class="ui avatar image" src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg"> <span>Arip Budiman</span>
+                </div>
+            </div>
+        </div>          
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Card",
+    props: ['taskData'],
+}
+</script>
+
+<style scoped>
+    .description{
+        color:#fff;
+    }
+    .description p {
+        font-size:11px;
+        letter-spacing: 0.5px;
+        font-weight: 500;
+        font-family: 'Montserrat', sans-serif;
+    }
+    .author{
+        color:#fff;
+        font-weight: bold;
+        font-family: 'Montserrat', sans-serif;
+    }
+    .author span{
+        font-size:11px;
+    }
+    .ui.card.card-item{
+        color:rgb(255, 255, 255);
+        background: rgba(0, 102, 90, 0.9);
+        box-shadow: 1px 2px 5px #000;
+    }
+</style>
