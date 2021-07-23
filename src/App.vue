700ab923cf233dc713797c1f39dc2243f1b6f656
nugrahakiat <nugrahakiatsaputra@gmail.com>
2021-07-23 22:00:27 +0700

    Login

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,41 @@
 <template>
         <div>
-            <!-- navbar -->
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-light" style="background-color: coral;" id="navbar">
-                    <a class="navbar-brand" style="color: whitesmoke;" href="#"><h3>Kanban</h3></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse navbar-nav" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#" id="nav-home">Home</a>
-                            </li>
-                            <li class="nav-item active">g
-                            <a class="nav-link" href="#" id="nav-add">Add Task</a>
-                            </li>
-                        </ul>
-                        <a class="nav-link active" href="#" id="nav-logout">Logout</a>
-                    </div>
-                </nav>
-            </div>
-            <!-- navbar -->
+            <Navbar></Navbar>
 
 
-            <div class="container d-flex justify-content-center align-items-center" style="padding-top: 7%;">
+            <!-- login -->
+            <div v-if="!isLogin" class="container d-flex justify-content-center align-items-center" style="padding-top: 7%;">
                 <div class="col-lg-6">
                     <div class="container d-flex justify-content-center align-items-center mt-4 mb-5 border-line"> <img src="./kanban.png" class="image"> </div>
                 </div>
                 <div class="col-lg-6">
-                    <!-- login -->
                     <div id="login-form">
                         <div class="container d-flex flex-column justify-content-center align-items-center">
-                            <form id="login-form" style="background: #fff; padding: 30px; border-radius: 10px; box-shadow: 0px 0px 10px 0px #bababa;">
+                            <form @submit.prevent="login" id="login-form" style="background: #fff; padding: 30px; border-radius: 10px; box-shadow: 0px 0px 10px 0px #bababa;">
                                 <h3 style="text-align: center; padding-bottom: 20px;">Login</h3>
                                 <div class="form-group">
-                                    <input type="email" class="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Enter Email" autocomplete="off">
+                                    <input v-model="user.email" type="email" class="form-control" id="email-login" aria-describedby="emailHelp" placeholder="Enter Email" autocomplete="off">
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control" id="password-login" placeholder="Password">
+                                    <input v-model="user.password" type="password" class="form-control" id="password-login" placeholder="Password">
                                 </div>
                                 <button type="submit" class="btn btn-primary btn-block" style="background-color: coral; color:whitesmoke;">Login</button>
-                                <p style="margin-top: 1rem;">Register new account? <a id="go-to-register" style="text-decoration: none; cursor: pointer; color:coral">register here</a></p>
+                                <p style="margin-top: 1rem;">Register new account? <a @click.prevent="formRegister" id="go-to-register" style="text-decoration: none; cursor: pointer; color:coral">register here</a></p>
                                 <p class="text-center">------- OR -------</p>
                                 <div class="container d-flex flex-column justify-content-center align-items-center g-signin2" data-onsuccess="onSignIn"></div>
                             </form>
                         </div> 
                     </div>
-                    <!-- login -->
+                </div>
+            </div>
+            <!-- login -->
 
-                    <!-- register form -->
+            <!-- register form -->
+            <div v-else-if="islogin === false && register === true" class="container d-flex justify-content-center align-items-center" style="padding-top: 7%;">
+                <div class="col-lg-6">
+                    <div class="container d-flex justify-content-center align-items-center mt-4 mb-5 border-line"> <img src="./kanban.png" class="image"> </div>
+                </div>
+                <div class="col-lg-6">
                     <div id="register-form">
                         <div class="container d-flex flex-column justify-content-center align-items-center">
                             <form id="register-form" style="background: #fff; padding: 30px; border-radius: 10px; box-shadow: 0px 0px 10px 0px #bababa;">
@@ -60,28 +47,28 @@
                                     <input type="password" class="form-control" id="password-register" placeholder="Your Password">
                                 </div>
                                 <button type="submit" class="btn btn-primary btn-block" style="background-color: coral; color:whitesmoke;">Submit</button>
-                                <p style="margin-top: 1rem;">Already have an account? <a id="go-to-login" style="text-decoration: none; cursor: pointer; color:coral">login here</a></p>
+                                <p style="margin-top: 1rem;">Already have an account? <a @click.prevent="formLogin" id="go-to-login" style="text-decoration: none; cursor: pointer; color:coral">login here</a></p>
                             </form>
                         </div>
                     </div>
-                    <!-- register form -->
 
                 </div>
             </div>
+            <!-- register form -->
         
         
             <!-- board -->
-            <div class="columns">
+            <div v-else class="columns">
                 <div class="column is-3">
                     <div class="card">
-                        <header class="card-header has-background-warning">
+                        <header class="card-header has-background-success">
                             <h4 class="card-header-title has-text-white">
                                 Materi
                             </h4>
                         </header>
                         <div class="card-content">
                             <div class="content">
-                                Jquery & Ajax
+                  o              Jquery & Ajax
                             </div>
                         </div>
                         <footer class="card-footer">
@@ -133,7 +120,7 @@
                 </div>
                 <div class="column is-3">
                     <div class="card">
-                        <header class="card-header has-background-info-dark">
+                        <header class="card-header has-background-success">
                             <h4 class="card-header-title has-text-white">
                                 Watch Video Recording
                             </h4>
@@ -151,7 +138,7 @@
                 </div>
                 <div class="column is-3">
                     <div class="card">
-                        <header class="card-header has-background-danger">
+                        <header class="card-header has-background-success">
                             <h4 class="card-header-title has-text-white">
                                 Done
                             </h4>
@@ -170,8 +157,64 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import Navbar from './components/Navbar'
+axios.defaults.baseURL = "http://localhost:3000"
 
+
+export default {
+ name: "App",
+ 
+ components: {Navbar},
+ data() {
+     return {
+         user: {
+             email: "",
+             password: ''
+         },
+         isLogin: false
+     }
+ },
+ methods: {
+     login(){
+        // console.log(this.user.email,"<<<<<<")
+        axios({
+            url: '/login',
+            method : 'POST',
+            // headers: {'Content-Type': 'application/json'},
+            data : this.user
+        })
+        .then(({data}) => {
+            //  console.log(data, '<<<<<<<<');
+             localStorage.setItem('access_token', data.access_token)
+             this.isLogin = true
+        })
+        .catch((err)=> {
+             console.log(err);
+        })
+     },
+
+     register(){
+            Axios({
+                method: 'POST',
+                url: `/register`,
+                data: {
+                    email: this.user.email,
+                    password: this.user.password
+                },
+            })
+            .then(data=>{
+                this.register = false
+            })
+            .catch((err)=> {
+             console.log(err);
+            })
+            .finally(()=>{
+                this.user.email = ''
+                this.user.password = ''
+            })
+        }
+ }
 }
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- navbar -->
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: coral;" id="navbar">
+            <a class="navbar-brand" style="color: whitesmoke;" href="#"><h3>Kanban</h3></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse navbar-nav" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li v-if="login" class="nav-item active">
+                    <a class="nav-link" href="#" @click.prevent="addTask" id="nav-add">Add Task</a>
+                    </li>
+                </ul>
+                <a v-if="login" class="nav-link active" href="#" @click.prevent="logout" id="nav-logout">Logout</a>
+            </div>
+        </nav>
+    </div>
+    <!-- navbar -->
+</template>
+
+<script>
+export default {
+    name: "navbar",
+    props: ['login'],
+    methods: {
+        addTask(){
+            this.$emit('addTask')
+        },
+        logout(){
+            this.$emit('logout')
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
